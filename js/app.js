56fc904010c031a997cbfe3bc9dc4791a8868cfc
Chris Anderson <cl.anderson8@gmail.com>
2016-02-26 22:59:39 -0600

    Thanks to that directive, I'm getting random css3 animation pathways.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('todoHunterApp', ['angular.filter', 'ngRoute'])
+var app = angular.module('todoHunterApp', ['angular.filter', 'ngRoute', 'dm.style'])
 
 //Setting Up routes
 app.config(['$routeProvider', function($routeProvider){
@@ -13,8 +13,25 @@ app.config(['$routeProvider', function($routeProvider){
 
 }]);
 
+/* the following directive thanks to deanmcphearson -- https://github.com/deanmcphearson/angular-inline-style */
+
+angular.module('dm.style', [])
+  .directive('style', ['$compile', function($compile) {
+    return {
+      restrict: 'E',
+      link: function postLink(scope, element) {
+        if (element.html()) {
+          var template = $compile('<style ng-bind-template="' + element.html() + '"></style>');
+          element.replaceWith(template(scope));
+        }
+      }
+    };
+  }]);
+
+//thanks dean!
+
 app.controller('MainController',
-  ['',
+  [
     function()
       {
 
@@ -22,8 +39,26 @@ app.controller('MainController',
 
         vm.playerName = null; //a string that the user can fill in.
 
-        vm.styles = []; //the class/id names that will apply to the animated divs
+        vm.styles = ["blue", "green", "red"]; //the class/id names that will apply to the animated divs
+
+        vm.rNum = [];
+
+        var getRandomNumString = function(){
+          var x = Math.floor(Math.random() * 700);
+          var str = (x + "px");
+          console.log(str);
+          vm.rNum.push(str);
+          console.log(vm.rNum);
+        };
+        while(vm.rNum.length < 50){
+          getRandomNumString();
+        }
+
+
+
 
         vm.todos = ["grocery shopping", "workout", "taxes", "laundry", "meal prep", "go to the post office", "return a library book", "drink heavily", "go to ______'s birthday party", "refill prescription", "call mom", "so and so's funeral", "that tv show I've been putting off", "read something besides buzzfeed", "cancel comcast", "vote for "] //the todos
+
+        vm.powerUps = ["slow down", "kill all"]
 
 }])
