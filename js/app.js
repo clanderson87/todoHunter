416b9935b12c038b84c7e1b4465e048683edccd6
Chris Anderson <cl.anderson8@gmail.com>
2016-02-29 15:56:27 -0600

    Working on rebuilding random paths.

--- js/app.js
+++ js/app.js
@@ -37,11 +37,17 @@ app.controller('MainController',
 
         var vm = this; //aliasing this
 
+        var delayTime = 30
+
         vm.playerName = null; //a string that the user can fill in.
 
         vm.styles = ["blue", "green", "red"]; //the class/id names that will apply to the animated divs
 
         vm.rNum = [];
+
+        vm.todos = ["grocery shopping", "workout", "taxes", "laundry", "meal prep", "go to the post office", "return a library book", "drink heavily", "go to ______'s birthday party", "refill prescription", "call mom", "so and so's funeral", "that tv show I've been putting off", "read something besides buzzfeed", "cancel comcast", "vote for "] //the todos
+
+        vm.powerUps = ["slow down", "kill all"]
 
         var getRandomNumString = function(){
           var x = Math.floor(Math.random() * 700);
@@ -51,15 +57,22 @@ app.controller('MainController',
           console.log(vm.rNum);
         };
 
-        while(vm.rNum.length < 30){
+        while(vm.rNum.length < delayTime){
           getRandomNumString();
+        }
+
+        //I need to run this function every delayTime seconds so new paths will be made.
+        var rebuildRNum = function(){
+          vm.rNum.forEach(function(number){
+            //need a regex to remove "px" from each index, then a method to convert the string into an int.
+            var x = Math.floor(Math.random() * 250);
+            var newNum = (Math.floor(number + x - Math.sqrt(x)) + "px");
+            vm.rNum.splice(0, number);
+            vm.rNum.push(newNum)
+          })
         }
 
 
 
-
-        vm.todos = ["grocery shopping", "workout", "taxes", "laundry", "meal prep", "go to the post office", "return a library book", "drink heavily", "go to ______'s birthday party", "refill prescription", "call mom", "so and so's funeral", "that tv show I've been putting off", "read something besides buzzfeed", "cancel comcast", "vote for "] //the todos
-
-        vm.powerUps = ["slow down", "kill all"]
 
 }])
